e577f60a6ebf61a52d541189e4c271103bd2fe13
Faypo <king1532168@gmail.com>
2020-07-16 20:12:23 +0800

    [add] player move controll/ camera move controll

---
+++ Assets/cameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    GameObject player;
+    public float posY;
+
+    private void Start()
+    {
+        this.player = GameObject.Find("player");
+    }
+
+    private void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y + posY, transform.position.z);
+    }
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2;
+
+    public float jumpForce;
+    public float walkForce;
+    public float maxWalkSpeed;
+
+    private void Start()
+    {
+        this.rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigidbody2.velocity.y == 0)
+        {
+            this.rigidbody2.AddForce(transform.up * this.jumpForce);
+
+        }
+
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rigidbody2.velocity.x);
+
+        if(speedx < this.maxWalkSpeed)
+        {
+            this.rigidbody2.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+    }
+}
