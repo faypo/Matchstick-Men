7f618d29007fc9a3a5da1e6bed95d330bcfaf6fd
Faypo <king1532168@gmail.com>
2020-07-23 20:23:29 +0800

    [add]player2 attack

---
+++ Assets/script/player2Attack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2Attack : MonoBehaviour
+{
+    public float time;
+    public int damage;
+
+    public KeyCode attackKey;
+    public Animator anima;
+    public PolygonCollider2D coll2d;
+
+    private void Start()
+    {
+        anima = GameObject.FindGameObjectWithTag("player2").GetComponent<Animator>();
+        coll2d = GetComponent<PolygonCollider2D>();
+    }
+
+    private void Update()
+    {
+        attack();
+    }
+
+    private void attack()
+    {
+        if (Input.GetKey(attackKey))
+        {
+            anima.SetTrigger("Attack");
+            coll2d.enabled = true;
+            StartCoroutine(disableHitBox());
+        }
+    }
+
+    IEnumerator disableHitBox()
+    {
+        yield return new WaitForSeconds(time);
+        coll2d.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().getHit(damage);
+        }
+    }
+}
